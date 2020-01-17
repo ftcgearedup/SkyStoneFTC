239fa5c13ab769c8f9417b86d3613549042ef55d
Karina <karinadavis@gmail.com>
2020-01-17 18:50:23 -0500

    undue changes made

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SkyStone/SkystoneRobot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SkyStone/SkystoneRobot.java
@@ -91,8 +91,6 @@ public class SkystoneRobot extends MechenumDriving {
         }
         if (colorSensor.red() >= 15) {
             intake(0);
-        } else{
-            stopMotors();
         }
 
 
